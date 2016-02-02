38ae9d6fe85224348526d6234178fba387b1f57f
Design Machinas <design@DesignMac1.local>
2016-02-02 16:29:44 +0100

    Split index and results

---
+++ Results.js
@@ -0,0 +1,109 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+import React, {
+  AppRegistry,
+  Component,
+  StyleSheet,
+  Text,
+  View,
+  ListView
+} from 'react-native';
+
+var REQUEST_URL = "https://jobs.github.com/positions.json";
+
+class Results extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {
+       dataSource: new ListView.DataSource({
+         rowHasChanged: (row1, row2) => row1 !== row2
+       }),
+       loaded: false,
+     };
+   }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  fetchData() {
+    fetch(REQUEST_URL)
+      .then((response) => response.json())
+      .then((responseData) => {
+        console.log(responseData)
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(responseData),
+          loaded: true,
+        });
+      })
+      .done();
+  }
+
+  render() {
+
+    if(!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderJobs}
+        style={styles.listView}
+      />
+    );
+  }
+
+  renderLoadingView() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Loading movies...
+        </Text>
+      </View>
+    );
+  }
+
+  renderJobs(job) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          {job.title}
+        </Text>
+        <Text style={styles.instructions}>
+          {job.location}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+    paddingTop: 75,  
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
+  },
+});
+
+module.exports = Results;
